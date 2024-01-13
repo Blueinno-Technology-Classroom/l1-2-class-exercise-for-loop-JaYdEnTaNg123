b60ba6867eedae73daafbd04f24aad558cfb3141
JaYdEnTaNg123 <tangjayden999@gmail.com>
2024-01-13 11:49:54 +0800

    git

--- main.py
+++ main.py
@@ -4,20 +4,27 @@ Q1:
 '''
 
 # TODO: Write your code here
-
+for i in range(0,1000001):
+    print(i)
 ##################################################
 '''
 Q2:
 '''
 
 # TODO: Write your code here
+x = 0
+for i in range(0,1000001):
+    x += i
 
+print(x)
 ##################################################
 '''
 Q3:
 '''
 
 # TODO: Write your code here
+for i in range(2,1000, 2):
+    print(i)
 
 ##################################################
 '''
@@ -25,6 +32,10 @@ Q4:
 '''
 
 # TODO: Write your code here
+print("Q4")
+for i in range(97,0,-3):
+    print(i)
+
 
 ##################################################
 '''
@@ -32,6 +43,8 @@ Q5:
 '''
 
 # TODO: Write your code here
+for i in range(0,11):
+    print(i*i)
 
 ##################################################
 '''
@@ -39,5 +52,37 @@ Q6:
 '''
 
 # TODO: Write your code here
+for i in range(5):
+    print("*****")
+
+##################################################
+
+'''
+Q7:
+'''
+
+# TODO: Write your code here
+for i in range(0,5):
+    print('*'*(i+1))
+
+##################################################
+
+'''
+Q8:
+'''
+
+# TODO: Write your code here
+for i in range(6,0,-1):
+    print('*'*(i))
+
+##################################################
+
+'''
+Q9:
+'''
+
+# TODO: Write your code here
+for i in range(1,6):
+    print(f"{i}"*i)
 
 ##################################################
